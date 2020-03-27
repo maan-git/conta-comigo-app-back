aa6f3c81fb214a92ff958f306b72c92860fcaf81
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-03-27 17:15:45 -0300

    make it better

---
+++ django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/manoel/Documents/help-me-corona/helpmecorona/django_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
