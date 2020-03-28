6ba921b70508af58c318aa1c051ea2aca1261e94
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-03-27 21:34:29 -0300

    Trabalhando na dockerização da aplicação.

--- helpmecorona/settings/dev.py
+++ helpmecorona/settings/dev.py
@@ -19,12 +19,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'contacomigo_dev',
-        'USER': 'contacomigo_user',
-        'PASSWORD': 'P@$$worD',
-        'HOST': 'localhost'
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
