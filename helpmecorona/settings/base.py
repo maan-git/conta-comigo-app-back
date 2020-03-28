670fac5f20b6bc12bbb67a886cf66b072ea9ee90
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-03-28 13:00:47 -0300

    Adição os arquivos Dockerfile e docker-compose.

--- helpmecorona/settings/base.py
+++ helpmecorona/settings/base.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
--- helpmecorona/settings/dev.py
+++ helpmecorona/settings/dev.py
@@ -18,12 +18,25 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+#         "USER": os.environ.get("SQL_USER", "user"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+#         # "HOST": os.environ.get("SQL_HOST", "localhost"),
+#         "HOST": '192.168.1.2',
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql_psycopg2"),
+        "NAME": os.environ.get("SQL_DATABASE", "contacomigo_dev"),
+        "USER": os.environ.get("SQL_USER", "contacomigo_user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "P@$$worD"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
