4209e0163c4a8dce76bcf19afe5b73a3ccc19399
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-03-31 22:27:01 -0300

    Realizando o método que faz o update do status do chamado.

--- help/views/help_request_view.py
+++ help/views/help_request_view.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import action
 import coreschema
 import coreapi
-from rest_framework.schemas import ManualSchema
+from rest_framework.schemas import ManualSchema, AutoSchema
 from rest_framework.viewsets import ModelViewSet
 from help.models.help_request import HelpRequest
 from requests import Request
@@ -12,6 +12,7 @@ from rest_framework.exceptions import ParseError
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import status
 from help.models.helping_status import HelpingStatus
+from help.models.help_request_status import HelpRequestStatus
 from help.models.helprequest_helpers import HelpRequestHelpers
 
 
@@ -83,3 +84,72 @@ class HelpRequestView(ModelViewSet):
         helping_user_relation.save()
 
         return Response(status=200)
+
+    """
+    • Criado → Cancelado
+    --• Criado → Em andamento--
+    • Em andamento → Cancelado    
+    • Em andamento → Finalizado
+    
+    Created = 1
+    InProgress = 20
+    Canceled = 99
+    Finished = 100
+    """
+
+    # Working on
+    @action(methods=["POST"],
+            detail=True,
+            url_path="updatestatushelp",
+            schema=ManualSchema(fields=[
+                coreapi.Field(
+                    "id",
+                    required=True,
+                    location="path",
+                    schema=coreschema.Integer()
+                ),
+                coreapi.Field(
+                    "status_id",
+                    required=True,
+                    location="path",
+                    schema=coreschema.Integer()
+                )
+            ])
+            )
+    def update_status_help(self, request: Request, pk):
+        helping_user_help_relation = self._validate_user_help_relation(request, pk)
+        print("Testing here:")
+        print(HelpRequestStatus.AllStatus.Canceled)
+        print(f"Request: {self.request.POST}")
+
+        if helping_user_help_relation.status_id == HelpRequestStatus.AllStatus.Created and \
+                request['status_id'] == HelpRequestStatus.AllStatus.Canceled:
+            helping_user_help_relation.status_id = HelpRequestStatus.AllStatus.Canceled
+            helping_user_help_relation.save()
+
+        elif helping_user_help_relation.status_id == HelpRequestStatus.AllStatus.InProgress and \
+                (request['status_id'] == HelpRequestStatus.AllStatus.Canceled or \
+                 request['status_id'] == HelpRequestStatus.AllStatus.Finished):
+            helping_user_help_relation.status_id = filter(lambda x: request['status_id'] == x,
+                                                          HelpRequestStatus.AllStatus)
+            helping_user_help_relation.save()
+        else:
+            raise ParseError(detail=_('You cannot make this operation'),
+                             code=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=200)
+
+    def _validate_user_relation(self, request: Request, pk):
+        helping_user_relation = HelpRequestHelpers.objects.filter(helper_user=request.user).first()
+
+        if not helping_user_relation:
+            raise ParseError(detail=_('You are not helping in this request'),
+                             code=status.HTTP_400_BAD_REQUEST)
+        return helping_user_relation
+
+    def _validate_user_help_relation(self, request: Request, pk):
+        helping_user_help_relation = HelpRequest.objects.filter(owner_user=request.user).first()
+        if not helping_user_help_relation:
+            raise ParseError(detail=_('You are not owner of this post'),
+                             code=status.HTTP_400_BAD_REQUEST)
+        return helping_user_help_relation
