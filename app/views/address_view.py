9f193aeba1a99f5a8006a12eeb4c28c1f3b69fa3
manoel <manoel.alves.de.almeida.neto@gmail.com>
2020-04-03 00:47:11 -0300

    Ajustando o processo de cadastro de endereço.

--- app/views/address_view.py
+++ app/views/address_view.py
@@ -1,3 +1,9 @@
+from rest_framework.decorators import action
+from rest_framework.schemas import ManualSchema, AutoSchema
+import coreschema
+import coreapi
+
+from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
 from app.models.address import Address
 from utils.views_utils import get_generic_read_serializer
@@ -10,3 +16,20 @@ class AddressView(ModelViewSet):
     def get_serializer_class(self):
         serializer_class = get_generic_read_serializer(Address, 1)
         return serializer_class
+
+    @action(methods=["post"],
+            detail=True,
+            url_path="addresbyzip",
+            schema=ManualSchema(description='ZIP code',
+                                fields=[
+                                    coreapi.Field(
+                                        "zip",
+                                        required=True,
+                                        location="path",
+                                        schema=coreschema.Integer(),
+                                        description='Zip'
+                                    )])
+            )
+    def get_address_by_zip(self, request: Request, pk):
+        address = Address.objects.filter(zip=request.data.zip).first()
+        return address
