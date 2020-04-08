100129d090d0dc4861980ba5062b5d0b35aaf51c
Sebastião Neto <sebastiao@landix.com.br>
2020-04-08 10:51:30 +0100

    Changes in scripts to import addresses to database only in production

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -4,4 +4,8 @@
 
 python manage.py migrate
 python manage.py loaddata help_0001_help_category help_0002_help_request_status help_0003_helping_status help_0004_help_request_cancel_reason app_0001_global_variable_type app_0002_brazilian_states app_0003_global_variable
-python manage.py loadaddresses app/data/addresses.zip --verbositylevel 0
+
+if [ "$DJANGO_SETTINGS_MODULE" = "helpmecorona.settings.prod" ]; then
+  # Use in production only
+  python manage.py loadaddresses app/data/addresses.zip --verbositylevel 0
+fi
--- entrypoint.sh
+++ entrypoint.sh
@@ -18,4 +18,9 @@ python manage.py makemigrations --merge
 python manage.py collectstatic --no-input --clear
 python manage.py loaddata help_0001_help_category help_0002_help_request_status help_0003_helping_status help_0004_help_request_cancel_reason app_0001_global_variable_type app_0002_brazilian_states app_0003_global_variable
 
+if [ "$DJANGO_SETTINGS_MODULE" = "helpmecorona.settings.prod" ]; then
+  # Use in production only
+  python manage.py loadaddresses app/data/addresses.zip --verbositylevel 0
+fi
+
 exec "$@"
