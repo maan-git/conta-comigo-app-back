58118fe22503acb391740c0e775e7b721fc06ac5
Sebastião Neto <sebastiao@landix.com.br>
2020-04-19 14:23:56 +0100

    Removed release script from heroky.yml since it's already being called from Docker-dev

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+py#!/bin/bash
 
 # Delpoyment tasks
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-#if [ "$DATABASE" = "postgres" ]
-#then
-#    echo "Waiting for postgres..."
-#
-#    while ! nc -z $SQL_HOST $SQL_PORT; do
-#      sleep 0.1
-#    done
-#
-#    echo "PostgreSQL started"
-#fi
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-#python manage.py flush --no-input
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py makemigrations --merge
